5d17fe6a07cd3c8c7b37a4390b135b8c374457e9
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-13 09:11:10 +0330

    Edit `description` and `keywords`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,17 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "IP Address Tracker",
-  description: "Search for any IP address or domain",
-  keywords: ["ip", "address", "tracker", "domain", "search"],
+  description:
+    "Search for any IP addresses or domains and see the key information and location",
+  keywords: [
+    "ip",
+    "address",
+    "tracker",
+    "domain",
+    "search",
+    "geolocation",
+    "ipgeolocation",
+  ],
 };
 
 export default function RootLayout({
