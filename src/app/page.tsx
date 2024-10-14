37e88c12551bfe80585fbc72aa97d5b2e92b3747
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-14 16:21:40 +0330

    Create bare `Map` component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Header";
+import Map from "@/components/Map";
 
 const Home = () => {
   return (
     <main className="relative w-full h-screen">
       <Header />
-      {/* Will replace with Map */}
-      <div className="bg-red-600 h-[65%] md:h-[75%]"></div>
+      <Map />
     </main>
   );
 };
---
+++ src/components/Map.tsx
@@ -0,0 +1,5 @@
+const Map = () => {
+  return <div className="bg-gray-400 h-[65%] md:h-[75%]"></div>;
+};
+
+export default Map;
