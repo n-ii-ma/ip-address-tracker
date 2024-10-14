7019c5614d4106909133e92a046457a29bcdf8e7
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-14 12:32:05 +0330

    Render `Header` and temp map replacement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Header from "@/components/Header";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="relative w-full h-screen">
+      <Header />
+      {/* Will replace with Map */}
+      <div className="bg-red-600 h-[65%] md:h-[75%]"></div>
+    </main>
+  );
 };
 
 export default Home;
