c95e350371fc4e757bb0f1fef5ad168a9a916ec5
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-14 17:51:44 +0330

    Render the map component with `react-leaflet`

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 const Map = () => {
-  return <div className="bg-gray-400 h-[65%] md:h-[75%]"></div>;
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="h-full"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
