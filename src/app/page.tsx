4afd9d0a41d5b2e516e66ade73ef108b589204e6
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-14 17:52:20 +0330

    Dynamically import the `Map` component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+import dynamic from "next/dynamic";
+
 import Header from "@/components/Header";
-import Map from "@/components/Map";
+
+// Dynamically import the Map component which is only rendered on the client side
+const DynamicMap = dynamic(() => import("@/components/Map"), {
+  ssr: false,
+  loading: () => (
+    <div className="flex items-center justify-center h-full">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-opacity-50"></div>
+    </div>
+  ),
+});
 
 const Home = () => {
   return (
     <main className="relative w-full h-screen">
       <Header />
-      <Map />
+      <section className="h-[65%] md:h-[75%]">
+        <DynamicMap />
+      </section>
     </main>
   );
 };
