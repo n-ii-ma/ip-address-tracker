d17423181f6ea1b3bbe4f3b60caef4bff8240014
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-14 18:08:30 +0330

    Edit map zoom

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,25 +4,23 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet-defaulticon-compatibility";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 const Map = () => {
   return (
     <MapContainer
       center={[51.505, -0.09]}
       zoom={13}
-      scrollWheelZoom={false}
+      zoomControl={false}
+      scrollWheelZoom
+      touchZoom
       className="h-full"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Marker position={[51.505, -0.09]} />
     </MapContainer>
   );
 };
