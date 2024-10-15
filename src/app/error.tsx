b513eaef70d6ee91438fd1238cce5c1672c9082a
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 09:24:07 +0330

    Create `error` file

---
+++ src/app/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  console.log(error);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-2xl">{error.message}</h2>
+      <button
+        className="mt-4 text-xl p-2 bg-black text-white rounded-md"
+        onClick={() => reset()}
+      >
+        Try Again
+      </button>
+    </div>
+  );
+};
+
+export default Error;
