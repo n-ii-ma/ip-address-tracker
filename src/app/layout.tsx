6f7373672e77d3ee497ae870b74d80693325d6f2
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 09:41:16 +0330

    Install `react-toastify`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./ui/globals.css";
 import { rubik } from "./ui/fonts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import type { Metadata } from "next";
 
@@ -25,7 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${rubik.className} antialiased`}>{children}</body>
+      <body className={`${rubik.className} antialiased`}>
+        {children}
+        <ToastContainer />
+      </body>
     </html>
   );
 }
