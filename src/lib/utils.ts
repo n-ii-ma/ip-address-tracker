0fcaeffd0ce8e5e2197e60c64eb3fafc05fa9eb1
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 12:08:09 +0330

    Create util function to check if IP/Domain is valid

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+export const isValidDomain = (domain: string) => {
+  const domainRegex = /^[a-zA-Z0-9-]{1,63}\.[a-zA-Z]{2,}$/;
+  return domainRegex.test(domain);
+};
+
+export const isIPValid = (ipAddress: string) => {
+  const ipv4Regex =
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  const ipv6Regex = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)$/;
+
+  return ipv4Regex.test(ipAddress) || ipv6Regex.test(ipAddress);
+};
