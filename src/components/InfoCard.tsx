a6628185660204c4b8de8b4b8d4744940ee503ff
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 14:19:18 +0330

    Replace dummy data with actual data

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,22 +1,16 @@
-// Dummy data
-const DATA = [
-  { title: "IP ADDRESS", value: "38.0.101.76" },
-  { title: "LOCATION", value: "Mountain View, CA" },
-  { title: "TIMEZONE", value: "UTC - 07:00" },
-  { title: "ISP", value: "SpaceX Starlink" },
-];
+import type { InfoCardProps } from "../types/Components";
 
-const InfoCard = () => {
+const InfoCard = ({ transformInfo }: InfoCardProps) => {
   return (
     <div className="flex flex-col justify-between items-center w-full p-6 gap-6 bg-white rounded-2xl z-[5000] md:flex-row md:w-auto">
-      {DATA.map((item, index) => (
+      {transformInfo.map((info, index) => (
         <div
           key={index}
           className="text-center md:min-h-20 md:text-left md:border-r md:border-gray-200 md:pr-10 md:last:border-0"
         >
-          <p className="text-xs text-darkGray mb-2">{item.title}</p>
+          <p className="text-xs text-darkGray mb-2">{info.title}</p>
           <h2 className="text-xl text-veryDarkGray font-medium">
-            {item.value}
+            {info.value}
           </h2>
         </div>
       ))}
