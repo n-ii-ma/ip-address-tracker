0b5041de4d0a82646131a1e19747f88003c0f22f
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 13:42:11 +0330

    Create API route to handle fetch

---
+++ src/app/api/ip/route.ts
@@ -0,0 +1,33 @@
+import { isValidDomain } from "@/lib/utils";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query") ?? "";
+  const baseURL = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.IPIFY_API_KEY}`;
+
+  // Create URL
+  const url = isValidDomain(query)
+    ? `${baseURL}&domain=${query}`
+    : `${baseURL}&ipAddress=${query}`;
+
+  try {
+    const res = await fetch(url);
+
+    // Check if the response status is not 2xx
+    if (!res.ok) {
+      const errorData = await res.json();
+      return Response.json(
+        {
+          message: errorData.messages || "Unknown error",
+        },
+        { status: res.status, statusText: res.statusText }
+      );
+    }
+
+    const data = await res.json();
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: "Error fetching data" }, { status: 500 });
+  }
+}
