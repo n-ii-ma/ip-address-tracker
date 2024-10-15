7ad92b9dbbbc8537cf7392dbf68ee69d0df5191c
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 14:47:11 +0330

    Make `Header` interactive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,32 @@ import patternBg from "../../public/images/pattern-bg.png";
 
 import SearchBar from "./SearchBar";
 import InfoCard from "./InfoCard";
+import InfoCardSkeleton from "./InfoCardSkeleton";
 
-const Header = () => {
+import type { IPInfo } from "../types/IPInfo";
+import type { HeaderProps, TransformedData } from "../types/Components";
+
+const Header = ({
+  ipInfo,
+  isLoading,
+  getSearchResults,
+  setSearchLoading,
+}: HeaderProps) => {
+  // Transform data
+  const transformData = (ipInfo: IPInfo): TransformedData[] => {
+    return [
+      { title: "IP ADDRESS", value: ipInfo.ip || "N/A" },
+      {
+        title: "LOCATION",
+        value: `${ipInfo.location.city || "N/A"}, ${
+          ipInfo.location.region || "N/A"
+        }`,
+      },
+      { title: "TIMEZONE", value: `UTC ${ipInfo.location.timezone || "N/A"}` },
+      { title: "ISP", value: ipInfo.isp || "N/A" },
+    ];
+  };
+
   return (
     <header className="relative flex flex-col items-center h-[35%] py-8 md:h-[25%]">
       {/* Background */}
@@ -27,8 +51,15 @@ const Header = () => {
 
       {/* Search and Result */}
       <section className="flex flex-col items-center w-[85%] mt-8 gap-6">
-        <SearchBar />
-        <InfoCard />
+        <SearchBar
+          getSearchResults={getSearchResults}
+          setSearchLoading={setSearchLoading}
+        />
+        {ipInfo && !isLoading ? (
+          <InfoCard transformInfo={transformData(ipInfo)} />
+        ) : (
+          <InfoCardSkeleton />
+        )}
       </section>
     </header>
   );
