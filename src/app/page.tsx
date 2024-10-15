115cc41cc67682b4dc0c2db99648bb81bbfa2afc
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 13:49:55 +0330

    Get IP info on mount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,46 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 
-import { getIPInfo } from "@/lib/fetchData";
+import { fetchIPInfo } from "@/lib/fetchData";
 
 import Header from "@/components/Header";
+import Loading from "./loading";
+
+import type { IPInfo } from "../types/IPInfo";
 
 // Dynamically import the Map component which is only rendered on the client side
 const DynamicMap = dynamic(() => import("@/components/Map"), {
   ssr: false,
-  loading: () => (
-    <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-opacity-50"></div>
-    </div>
-  ),
 });
 
-const Home = async () => {
-  // Get IP info
-  const { location } = await getIPInfo();
+const Home = () => {
+  // IP info state
+  const [ipInfo, setIpInfo] = useState<IPInfo | null>(null);
+
+  // Fetch IP info
+  useEffect(() => {
+    const getInfo = async () => {
+      const data = await fetchIPInfo();
+      setIpInfo(data);
+    };
+
+    getInfo();
+  }, []);
+
+  // Update state with search results
+  const getSearchResults = (result: IPInfo) => setIpInfo(result);
 
   return (
     <main className="relative w-full h-screen">
-      <Header />
+      <Header getSearchResults={getSearchResults} ipInfo={ipInfo} />
       <section className="h-[65%] md:h-[75%]">
-        <DynamicMap
-          position={location ? [location.lat, location.lng] : [51.505, -0.09]}
-        />
+        {ipInfo?.location ? (
+          <DynamicMap position={[ipInfo?.location.lat, ipInfo?.location.lng]} />
+        ) : (
+          <Loading />
+        )}
       </section>
     </main>
   );
