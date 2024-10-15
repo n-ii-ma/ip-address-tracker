109dd400717d6ce03777467dfaf5b3ed300a5879
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-10-15 13:48:04 +0330

    Change function name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ export const isValidDomain = (domain: string) => {
   return domainRegex.test(domain);
 };
 
-export const isIPValid = (ipAddress: string) => {
+export const isValidIP = (ipAddress: string) => {
   const ipv4Regex =
     /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   const ipv6Regex = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)$/;
